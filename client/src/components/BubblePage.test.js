b2a73fbf80d9893cbb2147898ceaaa393ffe09a1
Anthony Bratcher <tonybrat74@gmail.com>
2020-12-13 19:54:57 -0800

    mvp

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -2,6 +2,21 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
+import { fetchBubbles as mockFetchBubbles } from "../utils/fetchBubbles";
+jest.mock("../utils/fetchBubbles");
+
+const newBubbles = [
+  { code: { hex: "#6093ca" }, color: "blue", id: 10 },
+  { code: { hex: "#8a2be2" }, color: "blueviolet", id: 11 },
+];
+
+test("Fetches data and renders the bubbles", async () => {
   // Finish this test
+  mockFetchBubbles.mockResolvedValue(newBubbles);
+  // render(<BubblePage />);
+
+  // const bubbles = screen.findByText(/bubble/i);
+  // expect(await bubbles).toBeInTheDocument();
+  
+
 });
